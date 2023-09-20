150a34aebee533fe80585c17bfd237d977b29adc
mcladnier <142928223+mcladnier@users.noreply.github.com>
2023-09-20 13:00:20 -0500

    Update Conversation.cs

--- Conversation.cs
+++ Conversation.cs
@@ -3,13 +3,30 @@ using UnityEngine.SceneManagement;
 
 public class Conversation : MonoBehaviour
 {
-    public GameObject critter;
+    public Transform critter;
+    public int conversationControl;
+    public GameObject conversation1;
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("space") && SceneManager.GetActiveScene().name == "Critter Explores House")
+        //If the character is within a certain distance of the NPC
+        if(critter.position.x >= -1.04 && critter.position.x <= 0.97 && critter.position.y >= 1.58 && critter.position.y <= 4.12)
         {
-
+            //Further the conversation through the use of a variable
+            conversationControl = 1;
+        } else
+        {
+            //Otherwise, do not trigger the interaction
+            conversationControl = 0;
+        }
+        //If the conversation is allowed to be triggered, do so by clicking space and making the UI appear
+        if(Input.GetKey("space"))
+        {
+            if(conversationControl == 1)
+            {
+                conversation1.SetActive(true);
+            }
         }
     }
 }
